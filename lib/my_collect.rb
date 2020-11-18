e2337e5bc91b2e736bb4299c76712cc8e8576d70
Connor Holland <supersoccergoalie123@gmail.com>
2020-11-18 16:01:04 -0700

    commit tree cb8cdae0fc6ec1f1a8a4b238127cbde586438a05

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,30 @@
 
 
+def my_collect(collection)
+    count = 0
+    array = []
+    while count < collection.length
+        array << yield(collection[count])
+        count += 1
+    end
+    array
+end
+
+empty_array = []
+my_collect(empty_array) do |x|
+end
+
+# def my_collect(array)
+#     i = 0
+#     collection = []
+#     while i < array.length
+#       collection << yield(array[i])
+#       i += 1
+#     end
+#     collection
+#   end
+
+#   empty_array = []
+#   my_collect(empty_array) do |x|
+#     counter += 1
+#   end
